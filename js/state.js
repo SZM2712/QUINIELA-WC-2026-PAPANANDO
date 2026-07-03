fc75eb0ce97cd006f550a53d6a05a1124bd8b26f
SZM2712 <stefanzachrissonmontenegro@gmail.com>
2026-07-03 15:37:36 -0600

    Delete js directory

--- js/state.js
+++
@@ -1,9 +0,0 @@
-// Estado mutable compartido entre modulos (los bindings de ES modules son de solo lectura
-// para quien los importa, asi que el estado se agrupa en un solo objeto mutable).
-export const AppState={
-  CU:null,adm:false,uLk:false,uCd:'',uExt:false,
-  grSc:{},koSc:{},ST:{},BKT:{},
-  rThirdOv:null,rGr:{},rKo:{},rSt:{},
-  rrTab:'grupos',posTab:'grupos',
-  saveTimer:null,lastCd:'',lastNm:''
-};
